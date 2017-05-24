c5d264ced0335552450466df124ae02f974d4e91
Jorge Alonso <alu0100767803@ull.edu.es>
2017-05-24 16:43:28 +0000

    Cambiando extensiones

---
+++ inputs/input3.pl
@@ -0,0 +1,5 @@
+while a <= 10 do 
+    begin 
+        a := a + 2; 
+        b := b + 2 
+    end
--- mainfromfile.js
+++ mainfromfile.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var PEG = require("./arithmetics.js");
 var genCode = require("./gen-code.js");
 
-var fileName = process.argv[2] || 'input1';
+var fileName = process.argv[2] || 'inputs/input1.pl';
 
 fs.readFile(fileName, 'utf8', function (err,input) {
   if (err) { return console.log(err); }
