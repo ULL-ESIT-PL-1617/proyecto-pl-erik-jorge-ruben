ad69e2a6135b90eec4229339b66b8f0d352b2365
Jorge Alonso <alu0100767803@ull.edu.es>
2017-05-25 20:37:29 +0000

    Pruebas funcionando

--- test/test.js
+++ test/test.js
@@ -2,10 +2,12 @@ var chai = require('chai');
 var PEG = require('../js/arithmetics.js');
 var util = require('util');
 var expect = chai.expect;
-var blanket = require("blanket")({
-   /* options are passed as an argument object to the require statement */
+
+////// Esto producia el fallo
+/*var blanket = require("blanket")({
+   /* options are passed as an argument object to the require statement 
    "pattern": "/"
-});
+});*/
 
 function removeSpaces(str){
   return str.replace(/\s/g,'');;
@@ -14,16 +16,16 @@ function removeSpaces(str){
 describe("<---Test para comprobar que se cumple la salida esperada--->", function () {
   it("# var a; a := 2 + 2", function(){
     var tree = `
-                CodeBloc{symbolTable:{a:VarDec{name:'a',value:null}},code:BinOp{type:'=',left:Leaf{type:'ID',value:'a'},right:BinOp{type:'+',left:Leaf{type:'NUM',value:'2'},right:Leaf{type:'NUM',value:'2'}}}}
+                {"symbolTable":{"a":{"name":"a","value":null}},"code":{"type":"=","left":{"type":"ID","value":"a"},"right":{"type":"+","left":{"type":"NUM","value":"2"},"right":{"type":"NUM","value":"2"}}}}
                 `;
     tree = removeSpaces(tree);
-    console.log(util.inspect(PEG.parse('var a; a := 2 + 2')))
+     var res = removeSpaces(JSON.stringify(PEG.parse('var a; a := 2 + 2'), null, 2))
     expect(res).to.equal(tree);
   });
   
-  it("# if a < 10 then a := 15 else a := 45", function(){
+  it("# var a; if a < 10 then a := 15 else a := 45", function(){
     var tree = `
-                {"constantes":{},"variables":{},"procedimientos":{},"funciones":{},"sentencias":{"type":"if","condition":{"type":"<","left":"a","right":10},"then":{"type":":=","left":"a","right":15},"else":{"type":":=","left":"a","right":45}}}
+                {"symbolTable":{},"code":{"condition":{"type":"<","left":{"type":"ID","value":"a"},"right":{"type":"NUM","value":"10"}},"then":{"type":"=","left":{"type":"ID","value":"a"},"right":{"type":"NUM","value":"15"}},"else":{"type":"=","left":{"type":"ID","value":"a"},"right":{"type":"NUM","value":"45"}}}}
                 `;
     tree = removeSpaces(tree);
     var res = removeSpaces(JSON.stringify(PEG.parse('if a < 10 then a := 15 else a := 45'), null, 2))
@@ -31,7 +33,7 @@ describe("<---Test para comprobar que se cumple la salida esperada--->", functio
   });
   it("# while a <= 10 do begin a := a + 2; b := b + 2 end", function(){
     var tree = `
-                {"constantes":{},"variables":{},"procedimientos":{},"funciones":{},"sentencias":{"type":"while","condition":{"type":"<=","left":"a","right":10},"do":{"type":"block","code":{"1":{"type":":=","left":"a","right":{"type":"+","left":"a","right":2}},"2":{"type":":=","left":"b","right":{"type":"+","left":"b","right":2}}}}}}
+                {"symbolTable":{},"code":{"condition":{"type":"<=","left":{"type":"ID","value":"a"},"right":{"type":"NUM","value":"10"}},"do":{"1":{"type":"=","left":{"type":"ID","value":"a"},"right":{"type":"+","left":{"type":"ID","value":"a"},"right":{"type":"NUM","value":"2"}}},"2":{"type":"=","left":{"type":"ID","value":"b"},"right":{"type":"+","left":{"type":"ID","value":"b"},"right":{"type":"NUM","value":"2"}}}}}}
                 `;
     tree = removeSpaces(tree);
     var res = removeSpaces(JSON.stringify(PEG.parse('while a <= 10 do begin a := a + 2; b := b + 2 end'), null, 2))
@@ -39,7 +41,7 @@ describe("<---Test para comprobar que se cumple la salida esperada--->", functio
   });
   it("# function funcId (a, b, c){  const d = 1, e = 2;  var f, g, h;  begin    f := a + d;    g := b + e  end }; a := funcId(2, b, 2+2)", function(){
     var tree = `
-                {"constantes":{},"variables":{},"procedimientos":{},"funciones":{"funcId":{"constantes":{"d":1,"e":2},"variables":{"f":null,"g":null,"h":null},"procedimientos":{},"funciones":{},"sentencias":{"type":"block","code":{"1":{"type":":=","left":"f","right":{"type":"+","left":"a","right":"d"}},"2":{"type":":=","left":"g","right":{"type":"+","left":"b","right":"e"}}}},"Parametros":{"a":null,"b":null,"c":null}}},"sentencias":{"type":":=","left":"a","right":{"type":"functionCall","id":"funcId","parametros":{"1":2,"2":"b","3":{"type":"+","left":2,"right":2}}}}}
+                {"symbolTable":{"funcId":{"name":"funcId","params":{"a":{"name":"a","value":null},"b":{"name":"b","value":null},"c":{"name":"c","value":null}},"code":{"symbolTable":{"d":{"name":"d","value":{"type":"NUM","value":"1"}},"e":{"name":"e","value":{"type":"NUM","value":"2"}},"f":{"name":"f","value":null},"g":{"name":"g","value":null},"h":{"name":"h","value":null}},"code":{"1":{"type":"=","left":{"type":"ID","value":"f"},"right":{"type":"+","left":{"type":"ID","value":"a"},"right":{"type":"ID","value":"d"}}},"2":{"type":"=","left":{"type":"ID","value":"g"},"right":{"type":"+","left":{"type":"ID","value":"b"},"right":{"type":"ID","value":"e"}}}}}}},"code":{"type":"=","left":{"type":"ID","value":"a"},"right":{"id":{"type":"ID","value":"funcId"},"parametros":{"0":{"type":"NUM","value":"2"},"1":{"type":"ID","value":"b"},"2":{"type":"+","left":{"type":"NUM","value":"2"},"right":{"type":"NUM","value":"2"}}}}}}
                 `;
     tree = removeSpaces(tree);
     var res = removeSpaces(JSON.stringify(PEG.parse('function funcId (a, b, c){  const d = 1, e = 2;  var f, g, h;  begin    f := a + d;    g := b + e  end }; a := funcId(2, b, 2+2)'), null, 2))
