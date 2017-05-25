b709b09e71147942e83154f531ff7fc12dc091b7
Jorge Alonso <alu0100767803@ull.edu.es>
2017-05-25 12:23:03 +0000

    Corrigiendo gulpfile

--- output.js
+++ output.js
@@ -2,28 +2,7 @@ module.exports = () => {
   let e;
   let sym = {};
   try {
-    const w = 1;
-    const z = 20;
-    var a;
-    var b;
-    var c;
-
-    function foo(x, y, z) {
-      var l;
-      var m;
-      var n;
-      a = 2
-      if (a < 2 * (2 + 2)) {
-        a = b
-      } else {
-        b = c
-      }
-      while (a < b) {
-        a = b
-      }
-      return a;
-    }
-    a = a + foo(a, 3 + 3, 55);
+    a = 2 + 2;
     return sym;
   } catch (e) {
     let err = e.message.replace(/sym\.(\w+)/g, '$1');
