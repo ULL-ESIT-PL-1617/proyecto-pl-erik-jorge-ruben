16f977abdc991b264398412b498d6fdcca08d8e1
Jorge Alonso <alu0100767803@ull.edu.es>
2017-05-25 11:56:37 +0000

    Añadiendo gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+
+gulp.task('default', shell.task('node main'));
+
+gulp.task('test', shell.task('npm run test'));
+
+gulp.task('input1', shell.task('node mainfromfile.js inputs/input1.pl'));
+gulp.task('input2', shell.task('mode mainfromfile.js inputs/input2.pl'));
+gulp.task('input3', shell.task('mode mainfromfile.js inputs/input3.pl'));
+gulp.task('input4', shell.task('mode mainfromfile.js inputs/input4.pl'));
+gulp.task('input5', shell.task('mode mainfromfile.js inputs/input5.pl'));
