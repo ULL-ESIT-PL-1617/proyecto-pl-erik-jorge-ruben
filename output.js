2349f479fc42ee53662f9057e5da6f49c866c9ce
Jorge Alonso <alu0100767803@ull.edu.es>
2017-05-25 12:26:11 +0000

    Modificando input4

--- output.js
+++ output.js
@@ -2,7 +2,11 @@ module.exports = () => {
   let e;
   let sym = {};
   try {
-    a = 2 + 2;
+    const a = 1;
+    const b = 2;
+    const c = 3;
+    var d;
+    d = 2 + 2;
     return sym;
   } catch (e) {
     let err = e.message.replace(/sym\.(\w+)/g, '$1');
